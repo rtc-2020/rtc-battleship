633c30d7291b26f7060518507ed8bda4dbbee9f0
Karl Stolley <karl.stolley@gmail.com>
2020-10-21 12:00:15 -0500

    Generate and handle a redirect to a random game URL

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,12 @@ const utils = require('../lib/utilities.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const random_game_path = `/${utils.randomRoom(3,4,3)}`
+  res.redirect(random_game_path);
+});
+
+router.get('/:game([a-z]{3}-[a-z]{4}-[a-z]{3})', function(req, res, next) {
+  res.render('index', { title: `Game Room ${req.params["game"]}` });
 });
 
 module.exports = router;
