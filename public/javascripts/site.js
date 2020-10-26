38afe8b3eb37d39f326ac278c6500329978b045b
Karl Stolley <karl.stolley@gmail.com>
2020-10-26 14:49:44 -0500

    Handle client states and RTCPeerConnection setup

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -3,3 +3,14 @@ var sc = io.connect('/' + NAMESPACE);
 sc.on('message', function(data) {
   console.log('Message received: ' + data);
 });
+
+// Track client states
+var clientIs = {
+  makingOffer: false,
+  ignoringOffer: false,
+  polite: false
+}
+
+// Eventually, we will set up STUN servers here
+var rtc_config = null;
+var pc = new RTCPeerConnection(rtc_config);
