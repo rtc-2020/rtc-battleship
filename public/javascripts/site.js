9e0e6b2eb88846e2db9563d9a557f3b9ff5f47c1
Karl Stolley <karl.stolley@gmail.com>
2020-10-26 14:50:05 -0500

    Handle placeholder video elements

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -14,3 +14,16 @@ var clientIs = {
 // Eventually, we will set up STUN servers here
 var rtc_config = null;
 var pc = new RTCPeerConnection(rtc_config);
+
+// Let's handle video streams...
+// Set up simple media_constraints
+// (disable audio for classroom demo purposes)
+var media_constraints = { video: true, audio: false };
+// Handle self video
+var selfVideo = document.querySelector('#self-video');
+var selfStream = new MediaStream();
+selfVideo.srcObject = selfStream;
+// Handle peer video
+var peerVideo = document.querySelector('#peer-video');
+var peerStream = new MediaStream();
+peerVideo.srcObject = peerStream;
